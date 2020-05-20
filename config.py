0b7f36d98e0d3ef701c07ac4c1e3606879aafd61
milleniax <marsel.abdullin.00@mail.ru>
2020-05-20 18:59:15 +0300

    add config

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = '1072903861:AAEooMCno1LK2Ap7WdCaTaMPcZ_A2L7XWAc'
+PATH_CV2 = '/home/milleniax/tz_dsplabs/env/Lib/site-packages/cv2/data/'
+PATH_PHOTO = '/home/milleniax/tz_dsplabs/photo/'
+PATH_AUDIO = '/home/milleniax/tz_dsplabs/audio/'
